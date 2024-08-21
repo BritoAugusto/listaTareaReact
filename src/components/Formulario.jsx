48deba3324b9775f1b9eee15628e67f2d8f9b2f5
Augusto Brito <britoaugusto1@gmail.com>
2024-08-21 11:27:12 -0300

    agregamos la logica necesaria para crear una tarea y guardarla en el state itemTareas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,13 +1,29 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 // import {Form, Button} from "react-bootrstap";
+
+
 const Formulario = () => {
+    const [ItemTareas, setItemTareas] = useState([])
+    const [tarea, setTarea] = useState('')
+// const tomarTexto = (e)=>{
+//     console.log(e.target.value);
+//     setTarea(e.target.value);
+const handleSubmit = (e)=>{
+e.preventDefault();
+//guardar la tarea en ItemTareas
+//... operador spread
+setItemTareas([...ItemTareas, tarea])
+setTarea('');
+}
+
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex">
-          <Form.Control type="text" placeholder="Agrega una tarea" minLength={5} maxLength={200} required/>
+          <Form.Control type="text" placeholder="Agrega una tarea" onChange={(e)=> setTarea(e.target.value)} value={tarea}/>
           <Button variant="primary" type="submit">
             Enviar
           </Button>
