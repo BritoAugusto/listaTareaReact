bdd10e533214854a3cc54d833d73d1d1507124a0
Augusto Brito <britoaugusto1@gmail.com>
2024-08-21 10:34:07 -0300

    agregamos un color de fondo para el body

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "./App.css";
+ import "./App.css";
 import Formulario from "./components/Formulario";
 
 function App() {
   return (
     <>
       <main className="container my-5 text-center">
-        <h1>Bienvenido</h1>
-        <h2>Ingresa tus tareas</h2>
+        <h1 className="text-light">Bienvenido</h1>
+        <h2 className="text-light">Ingresa tus tareas</h2>
         <Formulario></Formulario>
       </main>
     </>
