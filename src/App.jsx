ba09e640707ace521c757159a095f787316baba6
Augusto Brito <britoaugusto1@gmail.com>
2024-09-18 11:39:44 -0300

    se limpio la estrucutra de listatareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
  import "./App.css";
-import Formulario from "./components/Formulario";
+
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <main className="container my-5 text-center">
         <h1 className="text-light">Bienvenido</h1>
         <h2 className="text-light">Ingresa tus tareas</h2>
-        <Formulario></Formulario>
+        
       </main>
     </>
   );
--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,9 +1,8 @@
-import { Button, ListGroup } from "react-bootstrap";
 
 const ItemTarea = () => {
     return (
       <div>
-        <ListGroup.Item className="d-flex justify-content-between">Tarea 1 <Button variant="danger">Borrar</Button> </ListGroup.Item>
+        
       </div>
     );
 };
