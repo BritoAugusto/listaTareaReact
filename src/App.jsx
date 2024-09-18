60c6a1c015495579f0d01069ff859d3508453f94
Augusto Brito <britoaugusto1@gmail.com>
2024-09-18 12:44:28 -0300

    se centró el texto de los titulos

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import FormularioTarea from "./components/FormularioTarea";
 function App() {
   return (
     <>
-      <main className="container my-5 text-center">
-        <h1 className="text-light">Bienvenido</h1>
-        <h2 className="text-light">Ingresa tus tareas</h2>
+      <main className="container my-5 ">
+        <h1 className="text-light text-center">Bienvenido</h1>
+        <h2 className="text-light text-center">Ingresa tus tareas</h2>
         <FormularioTarea></FormularioTarea>
       </main>
     </>
